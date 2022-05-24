ca26b2a208e83cb9ae25346d8c3a8c46c899ae48
george <george@DESKTOP-N0KQ8G4.localdomain>
2022-05-24 18:36:27 +0300

    sorted i/o

--- wf/__init__.py
+++ wf/__init__.py
@@ -3,7 +3,7 @@ Clean and Interpret Alignments
 """
 import subprocess
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Tuple
 
 from latch import small_task, workflow
 from latch.types import LatchFile
@@ -12,45 +12,141 @@ from latch.types import LatchFile
 @small_task
 def align_task(
         align_file: LatchFile,
-        config_file: LatchFile) -> LatchFile:
-
-    # defining the output and outfile_stem
-    out_put = Path("CIAlign.txt").resolve()
+        config_file: LatchFile) -> Tuple[LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile]:
 
     # The function to run
     _align_cmd = [
         "CIAlign",
         "--infile",
         align_file.local_path,
-        "--outfile_stem",
-        CIAlign,
         "--inifile",
-        str(Path(config_file).resolve())
+        str(Path(config_file).resolve()),
+        "--all",
     ]
 
     subprocess.run(_align_cmd, check=True)
 
-    return LatchFile(str(out_put), "latch:///CIAlign.txt")
+    return (LatchFile(str(Path("CIAlign_removed.txt")), "latch:///removed.txt"),
+            LatchFile(str(Path("CIAlign_log.txt")), "latch:///logs.txt"),
+            LatchFile(str(Path("CIAlign_cleaned.fasta")),
+                      "latch:///cleaned.fasta"),
+            LatchFile(str(Path("CIAlign_consensus.fasta")),
+                      "latch:///consensus_sequence.fasta"),
+            LatchFile(str(Path("CIAlign_with_consensus.fasta")),
+                      "latch:///cleanedWconsensus_sequence.fasta"),
+            LatchFile(str(Path("CIAlign_input.jpg")),
+                      "latch:///view_input.jpg"),
+            LatchFile(str(Path("CIAlign_output.jpg")),
+                      "latch:///view_output.jpg"),
+            LatchFile(str(Path("CIAlign_markup.jpg")),
+                      "latch:///view_markup.jpg"),
+            LatchFile(str(Path("CIAlign_logo_bar.jpg")),
+                      "latch:///logosBar.jpg"),
+            LatchFile(str(Path("CIAlign_logo_text.jpg")),
+                      "latch:///logosTxt.jpg"),
+            LatchFile(str(Path("CIAlign_input_coverage.jpg")),
+                      "latch:///CPlots_in.jpg"),
+            LatchFile(str(Path("CIAlign_output_coverage.jpg")),
+                      "latch:///CPlots_out.jpg"),
+            LatchFile(str(Path("CIAlign_input_similarity.tsv")),
+                      "latch:///stats_in.tsv"),
+            LatchFile(str(Path("CIAlign_output_similarity.tsv")),
+                      "latch:///stats_out.tsv")
 
 
-@workflow
-def CIAlign(align_file: LatchFile, config_file: LatchFile) -> LatchFile:
-    """The workflow is the implementation of the CIAlign tool.
+            )
+
+
+@ workflow
+def CIAlign(align_file: LatchFile, config_file: LatchFile) -> Tuple[LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile, LatchFile]:
+    """The workflow is the implementation of the CIAlign tool: Clean and analyse a multiple sequence alignment (MSA).
 
     ----
 
 
-    # Introduction 
+    # CIAlign
     CIAlign is a command line tool
     that allows users to remove specific issues from an MSA, visualise the MSA, and interpret the MSA.
-    The tool will remove regions of low coverage due to insertions, gaps
-    crop poorly aligned sequence ends and remove sequences that are too divergent or too short.
-    Compared to other tools, user have the opportunity to distinguish between gaps within 
-    the body of a sequence, that should be removed and gaps padding the ends of sequences of different lengths
-    CIAlign will first remove divergent sequences and consider shorter sequences towards the end of the
-    Cleaning stages. Mini alignments can be generated and visualised using coloured rectangles. 
-    Areas removed are marked up with different colors. The tool can also generate 
-    Sequence logos to show the information and base/amino acid content at each position
+
+    Users can perfom the followig functions:
+    ## Cleaning an MSA
+
+        - Remove insertions which are not present in the majority of sequences
+        - Remove sequences below a threshold number of bases or amino acids
+        - Crop poorly aligned sequence ends
+        - Remove columns containing only gaps
+        - Remove sequences above a threshold level percentage of divergence from the majority
+
+    ## Visualise alignments
+
+        - Generate image files showing the alignment before and after analysis using 
+        - showing which columns and rows have been removed
+        - Draw sequence logos
+        - Visualise coverage at each position in the alignment
+
+    ## Analyse alignment statistics 
+
+        - Generate a similarity matrix showing the percentage identity between each sequence pair 
+
+    # Usage 
+
+        For basic usage, user must input a fasta file with aligned sequences
+        and a config file: 
+
+        Downlaod the config file [here](/KatyBrown/CIAlign/blob/100bd7b87a889c120435ed91035e0e27c497f964/templates/ini_template.txt)
+
+        Edit the config file according to your needs. 
+
+        The config file included in the description returns analysis with 
+        default values
+
+        <br>
+        Downlaod an example fasta file [here](/KatyBrown/CIAlign/blob/100bd7b87a889c120435ed91035e0e27c497f964/example_files/example4.fasta)
+        for dummy use
+
+        Click __Input File__ to enter the input file and 
+
+        **Configs** to enter the config file
+
+        Clck **Launch Workflow** button to begin the rule
+
+    # Accessing files 
+
+        With a complete run, you can access the folowing files:
+
+    > "Cleaned" alignment as **cleaned.fasta** 
+
+    > Consensus Sequence
+    #### **consensus_sequence.fasta** containing consensus sequence only
+    #### **cleanedWconsensus_sequence.fasta** containing the cleaned alignment plus the consensus
+
+    <br>
+    > Mini Alignments 
+    #### **view_input.jpg:**  with the the input alignment
+    #### **view_output.jpg:** with the  the cleaned output alignment
+    #### **view_markup.jpg:** with the the input alignment with deleted rows and columns marked
+
+    <br>
+    > Logos 
+    ##### **logosBar.jpg:** the alignment represented as a bar chart
+    #### **logosTxt.jpg:**  the alignment represented as a standard sequence logo 
+
+    <br>
+    > Coverage Plots 
+    #### **CPlots_in.jpg** : image showing the input alignment coverage
+    ##### **CPlots_out.jpg** : image showing the output alignment coverage
+
+    <br>
+    > Similarity Matrices 
+    ##### **stats_in.tsv:**   similarity matrix for the input file
+    ##### **stats_out.tsv:**  similarity matrix for the output file
+
+    <br>
+    > Head to **Data** on the latch console to access various files
+
+
+
+
 
     Args:
 
@@ -73,8 +169,3 @@ def CIAlign(align_file: LatchFile, config_file: LatchFile) -> LatchFile:
     """
 
     return align_task(align_file=align_file, config_file=config_file)
-
-
-# if __name__ == "__main__":
-# CN(align_file=LatchFile(r"C:\Users\Steve Odete\latch_wf\clalign\data\example1.fasta"),
-#  config_file=LatchFile(r"C:\Users\Steve Odete\latch_wf\clalign\my_configs.ini"))
